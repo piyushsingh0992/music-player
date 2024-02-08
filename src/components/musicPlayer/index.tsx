eb4f02cb91d73ac33676ca77ac626b498208c663
piyushsingh0992 <piyushsingh0992@gmail.com>
2024-02-08 20:21:11 +0530

    changes

--- src/components/musicPlayer/index.tsx
+++ src/components/musicPlayer/index.tsx
@@ -1,33 +1,9 @@
 import React from "react";
 import Image from "next/image";
+import ProgressBar from "./progressBar";
 
-// ProgressBar component for the progress bar
-const ProgressBar = ({ currentTime, totalTime }: any) => {
-  // First, convert currentTime and totalTime to seconds for easier calculation
-  const currentTimeInSeconds = currentTime
-    .split(":")
-    .reduce((acc: number, time: number) => 60 * acc + +time, 0);
-  const totalTimeInSeconds = totalTime
-    .split(":")
-    .reduce((acc: number, time: number) => 60 * acc + +time, 0);
 
-  // Then, calculate the progress percentage
-  const progressPercentage = (currentTimeInSeconds / totalTimeInSeconds) * 100;
 
-  return (
-    <div className="flex items-center ">
-      <span className="text-xs text-gray-400 font-light">{currentTime}</span>
-
-      <div className="w-full bg-gray-300 rounded-full h-1.5 mx-4">
-        <div
-          className="bg-indigo-700 h-1.5 rounded-full hover:bg-indigo-500 cursor-pointer "
-          style={{ width: `${progressPercentage}%` }}
-        ></div>
-      </div>
-      <span className="text-xs text-gray-400 font-light">{totalTime}</span>
-    </div>
-  );
-};
 
 const Player = () => {
   return (
---
+++ src/components/musicPlayer/progressBar.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+interface ProgressBarProps {
+  currentTime: string;
+  totalTime: string;
+}
+
+const calculateProgressPercentage = (
+  currentTime: string,
+  totalTime: string
+): number => {
+  const timeInSeconds = (time: string): number =>
+    time.split(":").reduce((acc, time) => 60 * acc + Number(time), 0);
+
+  const currentTimeInSeconds = timeInSeconds(currentTime);
+  const totalTimeInSeconds = timeInSeconds(totalTime);
+
+  return (currentTimeInSeconds / totalTimeInSeconds) * 100;
+};
+
+const ProgressBar: React.FC<ProgressBarProps> = ({
+  currentTime,
+  totalTime,
+}) => {
+  const progressPercentage = calculateProgressPercentage(
+    currentTime,
+    totalTime
+  );
+
+  return (
+    <div className="flex items-center">
+      <span className="text-xs text-gray-400 font-light">{currentTime}</span>
+      <div className="w-full bg-gray-300 rounded-full h-1.5 mx-4">
+        <div
+          className="bg-indigo-700 h-1.5 rounded-full hover:bg-indigo-500 cursor-pointer"
+          style={{ width: `${progressPercentage}%` }}
+        ></div>
+      </div>
+      <span className="text-xs text-gray-400 font-light">{totalTime}</span>
+    </div>
+  );
+};
+
+export default ProgressBar;
